ac408c4807d8559c05ab1469fe086e287ecb6a48
zenit1925 <kostia-85-85@yandex.ru>
2022-12-15 16:52:23 +0300

    Task 1.1.5 version2 15/12/2022

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,12 +1,13 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.util.Util;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-import java.util.ArrayList;
+
+import javax.persistence.Query;
 import java.util.List;
+
 import static jm.task.core.jdbc.util.Util.getSessionFactory;
 
 
@@ -39,7 +40,7 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void dropUsersTable() {
-        String sql = "drop table if exists users";
+        String sql = "DROP TABLE if EXISTS users";
         try (Session session = getSessionFactory().openSession()) {
             Transaction transaction = session.beginTransaction();
 
@@ -57,11 +58,16 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void saveUser(String name, String lastName, byte age) {
         Transaction transaction = null;
-        try (Session session = Util.getSessionFactory().openSession()) {
+        try (Session session = getSessionFactory().openSession()) {
             transaction = session.beginTransaction();
             User user = new User(name, lastName, age);
-            session.save(user);
+            String sql = "INSERT INTO users (name, lastName, age) VALUES ('" + name + "', '" +
+                    lastName + "', " + age + ")";
+            session.createSQLQuery(sql)
+                    .addEntity(User.class)
+                    .executeUpdate();
             transaction.commit();
+
             System.out.println("User saved");
         } catch (HibernateException e) {
             if (transaction != null) {
@@ -75,7 +81,7 @@ public class UserDaoHibernateImpl implements UserDao {
         try (Session session = getSessionFactory().openSession()) {
             Transaction transaction = session.beginTransaction();
 
-            String sql = "delete from users where id=?";
+            String sql = "DELETE FROM users WHERE id = " + id;
 
             session.createSQLQuery(sql).executeUpdate();
             transaction.commit();
@@ -90,29 +96,37 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public List<User> getAllUsers() {
-        List<User> result = new ArrayList<>();
+        List userList = null;
+        Transaction transaction = null;
         try (Session session = getSessionFactory().openSession()) {
-            Transaction transaction = session.beginTransaction();
-            result = session.createQuery("from User", User.class).list();
-            session.getTransaction().commit();
+            transaction = session.beginTransaction();
+            String sql = "SELECT * FROM users";
+            Query query = session.createSQLQuery(sql).addEntity(User.class);
+            userList = query.getResultList();
+            for (Object user : userList) {
+                System.out.print(user.toString());
+            }
+            transaction.commit();
         } catch (HibernateException e) {
             e.printStackTrace();
         }
-        return result;
+        return userList;
     }
 
 
     @Override
     public void cleanUsersTable() {
-        String sql = "truncate table users";
+        Transaction transaction = null;
         try (Session session = getSessionFactory().openSession()) {
-            Transaction transaction = session.beginTransaction();
-
-            session.createSQLQuery(sql).executeUpdate();
-            session.getTransaction().commit();
-
+            transaction = session.beginTransaction();
+            String sql = "TRUNCATE TABLE users";
+            session.createSQLQuery(sql)
+                    .addEntity(User.class)
+                    .executeUpdate();
+            transaction.commit();
         } catch (HibernateException e) {
             e.printStackTrace();
         }
     }
 }
+
